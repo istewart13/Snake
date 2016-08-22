930d6a7bd88eb5c4e25bac7ab167f38493436b16
Iain Stewart <iain.stewart08@gmail.com>
2016-08-22 11:37:17 +0100

    very basic interfaces

--- app/src/main/java/snakeymcsnake/snake/MainActivity.java
+++ app/src/main/java/snakeymcsnake/snake/MainActivity.java
@@ -1,13 +1,27 @@
 package snakeymcsnake.snake;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton playButton = (ImageButton)findViewById(R.id.snakeImageButton);
+        playButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        i = new Intent(this, GameActivity.class);
+        startActivity(i);
     }
 }
